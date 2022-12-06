3c290f2ce9cf5eef0cfce6cf18d4f98556c9a55c
jinwood <jinw@protonmail.com>
2022-12-06 21:57:14 +0000

    Add basic layout, form

---
+++ islands/PlantForm.tsx
@@ -0,0 +1,7 @@
+export default function PlantForm() {
+  return (
+    <div>
+      I'm the plant form
+    </div>
+  )
+}
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,24 +1,26 @@
-import { Head } from "$fresh/runtime.ts";
-import Counter from "../islands/Counter.tsx";
+import Header from "../components/Navigation/Header.tsx";
+import Footer from "../components/Navigation/Footer.tsx";
+import PlantForm from "../islands/PlantForm.tsx";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Fresh App</title>
-      </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
-        </p>
-        <Counter start={3} />
-      </div>
-    </>
+    <main class="h-auto">
+      <Header />
+      <section class="bg-white dark:bg-green-900 h-full">
+        <div class="container mx-auto px-6 pt-28">
+          <h1 class="text-2xl font-semibold text-green-800 dark:text-white lg:text-4xl">
+            Hey
+          </h1>
+          <p class="text-green-800 dark:text-white lg:text-xl">
+            We plant trees here
+          </p>
+        </div>
+      </section>
+
+      <section class="bg-white dark:bg-green-900 h-full">
+        <PlantForm />
+      </section>
+      <Footer />
+    </main>
   );
 }
