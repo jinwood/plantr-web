d5895f7aec81d5e1184ed5e69f6e53ba7e1875e7
jinwood <jinw@protonmail.com>
2022-12-12 07:49:28 +0000

    Styles

--- compontents/forms/Plant.tsx
+++ compontents/forms/Plant.tsx
@@ -1,12 +1,24 @@
 import React from "react";
+import styles from "../../styles/PlantForm.module.css";
 
 export default function Plant() {
   return (
-    <form action="/send-data-here" method="post">
-      <label for="first">First name:</label>
-      <input type="text" id="first" name="first" />
-      <label for="last">Last name:</label>
-      <input type="text" id="last" name="last" />
+    <form action="/send-data-here" method="post" className={styles.container}>
+      <div>
+        <label for="first">Username:</label>
+        <input type="text" id="username" name="username" />
+      </div>
+
+      <div>
+        <label for="postUrl">Post URL:</label>
+        <input type="text" id="postUrl" name="postUrl" />
+      </div>
+
+      <div>
+        <label htmlFor="tip">Tip</label>
+        <input type="checkbox" name="tip" />
+      </div>
+
       <button type="submit">Submit</button>
     </form>
   );
